79510638746cbc5fbddf77420c16d6d2afb6e7fb
malit mohamut <60866464+mawlid1431@users.noreply.github.com>
2023-09-04 16:48:32 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Good Luck 💪🏾
+
+
+function brokenKeys(correctPhrase, typedPhrase) {
+  const brokenKeysArray = [];
+
+  for (let i = 0; i < correctPhrase.length; i++) {
+    if (correctPhrase[i] !== typedPhrase[i]) {
+      if (!brokenKeysArray.includes(correctPhrase[i])) {
+        brokenKeysArray.push(correctPhrase[i]);
+      }
+    }
+  }
+
+  return brokenKeysArray;
+}
+
+// Example usages:
+console.log(brokenKeys("fikrcamp bootcamp", "fikrcomp boorcamp")); // Output: ["a", "t"]
+console.log(brokenKeys("good night", "gppd night")); // Output: ["o"]
+
